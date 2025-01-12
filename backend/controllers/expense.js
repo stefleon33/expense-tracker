466c6c8fa17f496dd7f902e9b2a14ac7ffefe507
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2025-01-11 23:20:00 -0500

    expense schema, endpoints created

---
+++ backend/controllers/expense.js
@@ -0,0 +1,48 @@
+const expenseSchema = require("../models/expenseModel")
+
+exports.addExpense = async (req, res) => {
+    const {title, amount, category, description, date} = req.body;
+
+    const expense = expenseSchema({
+        title,
+        amount,
+        category,
+        description,
+        date
+    })
+
+    try {
+        //validations
+        if(!title || !category || !description || !date){
+            return res.status(400).json({message: 'All fields are requried!'})
+        }
+        if(amount <=0 || !amount === 'number'){
+            return res.status(400).json({message: 'Amount must be a positive number!'})
+        }
+        await expense.save()
+        res.status(200).json({message: 'Expense added!'})
+    } catch (error) {
+        res.status(500).json({message: 'Server Error'})
+    }
+    console.log(expense)
+}
+
+exports.getExpenses = async (req, res) => {
+    try { 
+        const expenses= await expenseSchema.find().sort({createdAt: -1})
+        res.status(200).json(expenses)
+    } catch (error) {
+        res.status(500).json({message: 'Server Error'})
+    }
+}
+
+exports.deleteExpense = async (req, res) => {
+    const {id} = req.params;
+    expenseSchema.findByIdAndDelete(id)
+        .then((expense) => {
+            res.status(200).json({message: 'Expense Deleted'})
+        })
+        .catch ((err) =>{
+            res.status(500).json({message: 'Server Error'})
+        })
+}

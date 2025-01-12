e64e34058081daea329977841a9a031d0d0b240e
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2025-01-11 22:35:57 -0500

    addIncome defined and imported

--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -1,7 +1,6 @@
+const { addIncome } = require('../controllers/income');
 const router = require('express').Router()
 
-router.get('/', (req, res) => {
-    res.send('hello Worlld')
-})
+router.post('/add-income', addIncome)
 
 module.exports = router

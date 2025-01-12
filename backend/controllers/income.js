647dd67104e9d8b7f6af905e1dd7b6f9299894a6
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2025-01-11 22:38:45 -0500

    Server error message

--- backend/controllers/income.js
+++ backend/controllers/income.js
@@ -22,7 +22,7 @@ exports.addIncome = async (req, res) => {
         await income.save()
         res.status(200).json({message: 'Income added!'})
     } catch (error) {
-
+        res.status(500).json({message: 'Server Error'})
     }
     console.log(income)
 }

137145f60c6b3cd6a5ef51ca3ddba509c877253d
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2025-01-11 23:11:27 -0500

    get-incomes and delete-income endpoints created

--- backend/controllers/income.js
+++ backend/controllers/income.js
@@ -16,7 +16,7 @@ exports.addIncome = async (req, res) => {
         if(!title || !category || !description || !date){
             return res.status(400).json({message: 'All fields are requried!'})
         }
-        if(amount <=0 ||!amount === 'number'){
+        if(amount <=0 || !amount === 'number'){
             return res.status(400).json({message: 'Amount must be a positive number!'})
         }
         await income.save()
@@ -25,4 +25,24 @@ exports.addIncome = async (req, res) => {
         res.status(500).json({message: 'Server Error'})
     }
     console.log(income)
+}
+
+exports.getIncomes = async (req, res) => {
+    try { 
+        const incomes= await incomeSchema.find().sort({createdAt: -1})
+        res.status(200).json(incomes)
+    } catch (error) {
+        res.status(500).json({message: 'Server Error'})
+    }
+}
+
+exports.deleteIncome = async (req, res) => {
+    const {id} = req.params;
+    incomeSchema.findByIdAndDelete(id)
+        .then((income) => {
+            res.status(200).json({message: 'Income Deleted'})
+        })
+        .catch ((err) =>{
+            res.status(500).json({message: 'Server Error'})
+        })
 }

2f6306347d6bc07618d5360c1c06e65c885d7bf8
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2025-01-11 22:00:59 -0500

    transaction route created

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const cors = require('cors');
 const { db } = require('./db/db');
+const { readdirSync } = require('fs')
 const app = express()
+
 require('dotenv').config()
 
 const Port = process.env.Port
@@ -9,6 +11,9 @@ const Port = process.env.Port
 //middlewares
 app.use(express.json())
 app.use(cors())
+
+//routes
+readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
 
 const server = () => {
     db()

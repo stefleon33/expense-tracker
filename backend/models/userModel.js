685a491daaea13ba5be4a03ac99e36414a211e5e
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2025-01-15 17:11:23 -0500

    match added to Email

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -15,7 +15,8 @@ const userSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
+        match: [/^\S+@\S+\.\S+$/, 'Invalid email address']
     }
 });
 

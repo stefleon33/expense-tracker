ad2548f8846889c64f71cbdbba8ecbc4b6b6f4c0
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2025-01-15 17:07:31 -0500

    More informative error message

--- backend/db/db.js
+++ backend/db/db.js
@@ -6,7 +6,7 @@ const db = async() => {
         await mongoose.connect(process.env.MONGO_URL)
         console.log('DB Connection Successful')
     } catch (error) {
-        console.log('DB Connection Error')
+        console.log('DB Connection Error: ', error.message)
     }
 }
 

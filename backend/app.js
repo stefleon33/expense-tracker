64b8517b31e3d3fbeccce8a6f1723d8775afe204
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2025-01-16 23:31:36 -0500

    Organized middlewares

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,20 @@
 const express = require('express')
 const cors = require('cors');
 const { db } = require('./db/db');
-const { readdirSync } = require('fs')
-const app = express()
+const { readdirSync } = require('fs');
 
+const passport = require('passport');
+require('./passport');
+
+const app = express();
 require('dotenv').config()
 
 const port = process.env.PORT
 
 //middlewares
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
+app.use(passport.initialize());
 
 //routes
 readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))

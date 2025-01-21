5b682a53f3ae15147f8916e86343574fa76280c1
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2025-01-20 22:59:50 -0500

    activeState added to navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,19 @@
+import React, { useState } from 'react';
 import styled from "styled-components";
 import bg from './img/bg.png'
 import { MainLayout } from "./styles/Layouts";
 import Orb from "./Components/Orb/Orb";
+import Navigation from "./Components/Navigation/Navigation";
+
 
 function App() {
+  const [active, setActive] = useState(1)
+  
   return (
     <AppStyled bg={bg} className="App">
       <Orb />
       <MainLayout>
-        <h1>Hello</h1>
+        <Navigation active={active} setActive={setActive}/>
       </MainLayout>
     </AppStyled>
   );

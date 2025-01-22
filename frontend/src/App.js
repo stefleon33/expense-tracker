999e1613a83ae8b10641eb1831c93e94b8ebad37
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2025-01-22 00:06:35 -0500

    displayData called for Dashboard, Incomes and Expenses

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,10 +4,29 @@ import bg from './img/bg.png'
 import { MainLayout } from "./styles/Layouts";
 import Orb from "./Components/Orb/Orb";
 import Navigation from "./Components/Navigation/Navigation";
-
+import Dashboard from './Components/Dashboard/Dashboard';
+import Incomes from './Components/Incomes/Incomes';
+import Expenses from './Components/Expenses/Expenses';
 
 function App() {
   const [active, setActive] = useState(1)
+
+  const global = useGlobalContext()
+
+  const displayData = () => {
+    switch(active){
+      case 1:
+        return <Dashboard />
+      case 2:
+        return <Dashboard />
+      case 3:
+        return <Incomes />
+      case 4:
+        return <Expenses />
+      default: 
+        return <Dashboard />
+    }
+  }
 
   const orbMemo= useMemo(() => {
     return <Orb />
@@ -18,6 +37,9 @@ function App() {
       {orbMemo}
       <MainLayout>
         <Navigation active={active} setActive={setActive} />
+        <main>
+          {displayData()}
+        </main>
       </MainLayout>
     </AppStyled>
   );
@@ -27,6 +49,17 @@ const AppStyled = styled.div`
   height: 100vh;
   background-image: url(${props => props.bg});
   position: relative;
+  main{
+    flex: 1;
+    background: rgba(252, 246, 249, 0.78);
+    backdrop-filter: blur(4.5px);
+    border-radius: 32px; 
+    overflow: auto;
+    overflor-x: hidden;
+    &::-webkit-scrollbar{
+      width: 0;
+    }
+  }
 `;
 
 export default App;

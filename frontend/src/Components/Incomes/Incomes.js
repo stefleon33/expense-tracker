583ca1173273cad25a09a43cd608b60210297054
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2025-01-23 16:38:31 -0500

    Income Icons mapped

--- frontend/src/Components/Incomes/Incomes.js
+++ frontend/src/Components/Incomes/Incomes.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 import { InnerLayout } from '../../styles/Layouts';
 import { useGlobalContext } from '../../contexts/globalContext';
 import Form from '../Form/Form';
+import IncomeItem from '../IncomeItem/IncomeItem';
 
 function Incomes() {
     const {addIncome,incomes,getIncomes} = useGlobalContext()
@@ -20,7 +21,18 @@ function Incomes() {
                         <Form></Form>
                     </div>
                     <div className='incomes'>
-
+                        {incomes.map((income) => {
+                            const {_id, title, amount, date, category, description} = income;
+                            return <IncomeItem
+                                key={_id}
+                                id={_id}
+                                title={title}
+                                description={description}
+                                amount={amount} date={date}
+                                category={category}
+                                indicatorColor="var(--color-green)"
+                            />
+                        })}
                     </div>
                 </div>
             </InnerLayout>

14999f55288641b93c23b651ff12c27a1d866671
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2025-01-30 23:04:23 -0500

    amount-con created

--- frontend/src/Components/Dashboard/Dashboard.js
+++ frontend/src/Components/Dashboard/Dashboard.js
@@ -2,8 +2,11 @@ import React from 'react'
 import styled from 'styled-components'
 import { InnerLayout } from '../../styles/Layouts';
 import Chart from '../Chart/Chart';
+import { dollar } from '../../utils/Icons'
+import { useGlobalContext } from '../../contexts/globalContext';
 
 function Dashboard() {
+    const {totalExpenses, totalIncome, totalBalance} = useGlobalContext()
     return (
         <DashboardStyled>
             <InnerLayout>
@@ -11,6 +14,26 @@ function Dashboard() {
                 <div className='stats-con'>
                     <div className='chart-con'>
                         <Chart />
+                        <div className='amount-con'>
+                            <div className='income'>
+                                <h2>Total Income</h2>
+                                <p>
+                                    {dollar} {totalIncome()}
+                                </p>
+                            </div>
+                            <div className='expense'>
+                                <h2>Total Expenses</h2>
+                                <p>
+                                    {dollar} {totalExpenses()}
+                                </p>
+                            </div>
+                            <div className='balance'>
+                                <h2>Total Balance</h2>
+                                <p>
+                                    {dollar} {totalBalance()}
+                                </p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </InnerLayout>

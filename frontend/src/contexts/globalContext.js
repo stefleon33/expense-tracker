5945ae5381726a063afac2f97dada44af05129a9
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2025-01-31 09:19:13 -0500

    render only 3 recent transactions

--- frontend/src/contexts/globalContext.js
+++ frontend/src/contexts/globalContext.js
@@ -78,6 +78,8 @@ export const GlobalProvider = ({children}) => {
         history.sort((a, b) => {
             return new Date(b.createdAt) - new Date(a.createdAt)
         })
+
+        return history.slice(0,3)
     }
 
     return (
